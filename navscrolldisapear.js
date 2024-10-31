a464e3e8dc5d24dca69852693020e0ce51c6d40f
astrumaegiss <isaac@havas.net.au>
2024-11-01 00:11:52 +1100

    blue

--- navscrolldisapear.js
+++ navscrolldisapear.js
@@ -1,9 +1,15 @@
 function openNav() {
-  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("mySidenav").style.width = "250px";
   document.getElementById("main").style.marginLeft = "250px";
+  document.body.style.backgroundColor = "rgba(30,30,46,.95)";
+  var element = document.getElementById("menu");
+  element.classList.toggle("hidden");
 }
 
 function closeNav() {
-  document.getElementById("mySidebar").style.width = "0";
-  document.getElementById("main").style.marginLeft= "0";
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+  document.body.style.backgroundColor = "var(--ctp-mocha-base)";
+  var element = document.getElementById("menu");
+  element.classList.toggle("shown");
 }
